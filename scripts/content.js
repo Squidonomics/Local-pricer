a87492e3275b429a1f9566571c6b78ebbdea02f7
Brian <eigthmonthstothemoon@gmail.com>
2023-09-30 02:56:50 -0400

    feat: Add new price to html

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,16 @@
 window.addEventListener("load", function(event) {
     var spans = document.querySelectorAll(".a-offscreen");
     console.log(spans);
-    for (let i = 0; i < spans.length; i++) {
-        item = spans.item(i).innerHTML;
-        console.log(item);
-    }    
+    spans.forEach((span) => {
+        item = span.innerHTML;
+        let updated = item.replace(/^[$]/g, "");
+        let updatedPrice = parseFloat(updated) * 1.095;
+        let roundedPrice = updatedPrice.toFixed(2);
+        let localPrice = document.createElement('p');
+        localPrice.innerHTML = "$" + roundedPrice;
+        span.after(localPrice);
+        console.log(roundedPrice);
+    })   
     console.log("This fired");
 });
 
