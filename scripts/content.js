e42073f4f3761077dc45ad418038128359020d18
Squid <eigthmonthstothemoon@gmail.com>
2023-10-04 19:41:10 -0400

    feat: Switch for different websites

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,52 @@
-window.addEventListener("load", function(event) {
-    var spans = document.querySelectorAll(".a-offscreen");
-    console.log(spans);
-    spans.forEach((span) => {
-        item = span.innerHTML;
-        let updated = item.replace(/^[$]/g, "");
-        let updatedPrice = parseFloat(updated) * 1.095;
-        let roundedPrice = updatedPrice.toFixed(2);
-        let localPrice = document.createElement('p');
-        localPrice.innerHTML = "$" + roundedPrice;
-        span.after(localPrice);
-        console.log(roundedPrice);
-    })   
-    console.log("This fired");
-});
+const href = window.location.href;
+const findTerm = (term) => {
+  if (href.includes(term)){
+    return href;
+  }
+};
+switch(href) {
+    case findTerm('amazon'):
+        amazonLocalize()
+        break;
+    case findTerm('bestbuy'):
+        bestBuyLocalize()
+        break;
+    default:
+        console.log("Missed both switch");
+}
 
-console.log("Outside hit");
+function amazonLocalize() {
+    window.addEventListener("load", function(event) {
+        var spans = document.querySelectorAll(".a-offscreen");
+        console.log(spans);
+        spans.forEach((span) => {
+            item = span.innerHTML;
+            let updated = item.replace(/^[$]/g, "");
+            let updatedPrice = parseFloat(updated) * 1.095;
+            let roundedPrice = updatedPrice.toFixed(2);
+            let localPrice = document.createElement('p');
+            localPrice.innerHTML = "$" + roundedPrice;
+            span.after(localPrice);
+            console.log(roundedPrice);
+        })   
+        console.log("This fired");
+    });
+}  
+
+function bestBuyLocalize() {
+    window.addEventListener("load", function(event) {
+        var spans = document.querySelectorAll(".sr-only");
+        console.log(spans);
+        spans.forEach((span) => {
+            item = span.innerHTML;
+            let updated = item.replace(/^[$]/g, "");
+            let updatedPrice = parseFloat(updated) * 1.095;
+            let roundedPrice = updatedPrice.toFixed(2);
+            let localPrice = document.createElement('p');
+            localPrice.innerHTML = "$" + roundedPrice;
+            span.after(localPrice);
+            console.log(roundedPrice);
+        })   
+        console.log("This fired");
+    });
+} 
