a6e200a573a793803e4183a46e556fe537bb2f46
Squid <eigthmonthstothemoon@gmail.com>
2023-10-18 22:35:16 -0400

    feat: Localize prices on landing pages

--- scripts/content.js
+++ scripts/content.js
@@ -7,6 +7,7 @@ const findTerm = (term) => {
 switch (href) {
     case findTerm("amazon"):
         amazonLocalize();
+        amazonLandingLocalize();
         break;
     default:
         console.log("Missed both switch");
@@ -39,6 +40,47 @@ function amazonLocalize() {
                     console.log("Nothing here");
                 } else {
                     span.after(createPrettyPrice(roundedPrice));
+                    console.log(roundedPrice);
+                }
+            });
+        });
+        const internalDivElements = document.querySelectorAll(
+            'span[aria-hidden="true"]'
+        );
+        for (const internalDivElement of internalDivElements) {
+            internalDivElement.parentNode.removeChild(internalDivElement);
+        }
+        console.log("This fired");
+    });
+}
+
+function amazonLandingLocalize() {
+    window.addEventListener("load", function (event) {
+        var spans = document.querySelectorAll(".Price__price__LKpWT");
+        console.log(spans);
+        spans.forEach((span) => {
+            item = span.innerHTML;
+            let updated = item.replace(/^[$]/g, "");
+            chrome.storage.sync.get(["key"]).then((result) => {
+                const dictionary = {
+                    "Knoxville": 1.095,
+                    "San Diego": 1.0775,
+                    "Los Angeles": 1.095,
+                    "LA": 1.095,
+                    "New York City": 1.088,
+                    "NYC": 1.088,
+                    "Chicago": 1.1,
+                    "Houston": 1.0825,
+                    "Phoenix": 1.086
+                }
+                const taxValue = dictionary[result.key];
+                console.log(taxValue);
+                let updatedPrice = parseFloat(updated) * taxValue;
+                let roundedPrice = updatedPrice.toFixed(2);
+                if (isNaN(roundedPrice)) {
+                    console.log("Nothing here");
+                } else {
+                    span.after(createLandingPagePrice(roundedPrice));
                     console.log(roundedPrice);
                 }
             });
